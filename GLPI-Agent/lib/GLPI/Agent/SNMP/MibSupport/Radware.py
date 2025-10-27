242786d1fd7e1b709691f3c83ec823933fda002d
Eshwar Thota <162366958+Eshwar26@users.noreply.github.com>
2025-10-27 16:30:08 +0530

    Update Radware.py

--- GLPI-Agent/lib/GLPI/Agent/SNMP/MibSupport/Radware.py
+++ GLPI-Agent/lib/GLPI/Agent/SNMP/MibSupport/Radware.py
@@ -1,140 +1,88 @@
-package GLPI::Agent::SNMP::MibSupport::Radware;
+# radware_mib.py
+from glpi_agent_snmp_template import MibSupportTemplate
+from glpi_agent_tools import get_canonical_string, get_regexp_oid_match, get_canonical_mac_address
 
-use strict;
-use warnings;
-
-use parent 'GLPI::Agent::SNMP::MibSupportTemplate';
-
-use GLPI::Agent::Tools;
-use GLPI::Agent::Tools::SNMP;
-
-use constant    enterprises => '.1.3.6.1.4.1' ;
+ENTERPRISES = '.1.3.6.1.4.1'
 
 # ALTEON-ROOT-MIB
-use constant    alteon  => enterprises . '.1872';
-use constant    awsswitch   => alteon . '.2.5';
+ALTEON = ENTERPRISES + '.1872'
+AWSSWITCH = ALTEON + '.2.5'
 
 # ALTEON-CHEETAH-SWITCH-MIB
-use constant    agent   => awsswitch . '.1';
+AGENT = AWSSWITCH + '.1'
+AGENT_CONFIG = AGENT + '.1'
+AGENT_INFO = AGENT + '.3'
 
-use constant    agentConfig => agent . '.1';
-use constant    agentInfo   => agent . '.3';
+AG_SYSTEM = AGENT_CONFIG + '.1'
+AG_MGMT = AGENT_CONFIG + '.9'
 
-use constant    agSystem    => agentConfig . '.1';
-use constant    agMgmt      => agentConfig . '.9';
+HARDWARE = AGENT_INFO + '.1'
 
-use constant    hardware    => agentInfo . '.1';
+AG_PLATFORM_IDENTIFIER = AG_SYSTEM + ".77.0"
+AG_MGMT_CUR_CFG_IPADDR = AG_MGMT + ".1.0"
 
-use constant    agPlatformIdentifier    => agSystem . ".77.0";
+HW_MAINBOARD_NUMBER = HARDWARE + '.6.0'
+HW_MAINBOARD_REVISION = HARDWARE + '.7.0'
+HW_MAC_ADDRESS = HARDWARE + '.13.0'
+HW_SERIAL_NUMBER = HARDWARE + '.18.0'
+HW_PLD_FIRMWARE_VERSION = HARDWARE + '.21.0'
+HW_VERSION = HARDWARE + '.30.0'
 
-use constant    agMgmtCurCfgIpAddr      => agMgmt . ".1.0";
 
-use constant    hwMainBoardNumber       => hardware . '.6.0';
-use constant    hwMainBoardRevision     => hardware . '.7.0';
-use constant    hwMACAddress            => hardware . '.13.0';
-use constant    hwSerialNumber          => hardware . '.18.0';
-use constant    hwPLDFirmwareVersion    => hardware . '.21.0';
-use constant    hwVersion               => hardware . '.30.0';
+class Radware(MibSupportTemplate):
+    mib_support = [
+        {"name": "alteon-radware", "sysobjectid": get_regexp_oid_match(ALTEON)}
+    ]
 
-our $mibSupport = [
-    {
-        name        => "alteon-radware",
-        sysobjectid => getRegexpOidMatch(alteon)
-    }
-];
+    def get_firmware(self) -> str:
+        return get_canonical_string(self.get(HW_PLD_FIRMWARE_VERSION))
 
-sub getFirmware {
-    my ($self) = @_;
+    def get_ip(self) -> str:
+        device = self.device
+        if device.IPS:
+            return None
+        ip = get_canonical_string(self.get(AG_MGMT_CUR_CFG_IPADDR))
+        return ip
 
-    return getCanonicalString($self->get(hwPLDFirmwareVersion));
-}
+    def get_manufacturer(self) -> str:
+        device = self.device
+        if device.MANUFACTURER:
+            return None
+        return "Radware"
 
-sub getIp {
-    my ($self) = @_;
+    def get_model(self) -> str:
+        model = get_canonical_string(self.get(AG_PLATFORM_IDENTIFIER))
+        return f"Alteon {model}"
 
-    my $device = $self->device
-        or return;
+    def get_serial(self) -> str:
+        return get_canonical_string(self.get(HW_SERIAL_NUMBER))
 
-    return if $device->{IPS};
+    def run(self):
+        device = self.device
 
-    my $ip = getCanonicalString($self->get(agMgmtCurCfgIpAddr))
-        or return;
+        mbnum = get_canonical_string(self.get(HW_MAINBOARD_NUMBER))
+        mbrev = get_canonical_string(self.get(HW_MAINBOARD_REVISION))
+        if mbnum and mbrev:
+            firmware = {
+                "NAME": f"{device.MODEL} {mbnum} mainboard",
+                "DESCRIPTION": f"{device.MODEL} {mbnum} mainboard revision",
+                "TYPE": "mainboard",
+                "VERSION": mbrev,
+                "MANUFACTURER": "Radware"
+            }
+            device.add_firmware(firmware)
 
-    return $ip;
-}
+        hw_version = get_canonical_string(self.get(HW_VERSION))
+        if hw_version:
+            firmware = {
+                "NAME": f"{device.MODEL} hardware",
+                "DESCRIPTION": f"{device.MODEL} hardware revision",
+                "TYPE": "device",
+                "VERSION": hw_version,
+                "MANUFACTURER": "Radware"
+            }
+            device.add_firmware(firmware)
 
-sub getManufacturer {
-    my ($self) = @_;
-
-    my $device = $self->device
-        or return;
-
-    return if $device->{MANUFACTURER};
-
-    return 'Radware';
-}
-
-sub getModel {
-    my ($self) = @_;
-
-    my $model = getCanonicalString($self->get(agPlatformIdentifier))
-        or return;
-
-    return "Alteon $model";
-}
-
-sub getSerial {
-    my ($self) = @_;
-
-    return getCanonicalString($self->get(hwSerialNumber));
-}
-
-sub run {
-    my ($self) = @_;
-
-    my $device = $self->device
-        or return;
-
-    my $mbnum = getCanonicalString($self->get(hwMainBoardNumber));
-    my $mbrev = getCanonicalString($self->get(hwMainBoardRevision));
-    if ($mbnum && $mbrev) {
-        my $firmware = {
-            NAME            => "$device->{MODEL} $mbnum mainboard",
-            DESCRIPTION     => "$device->{MODEL} $mbnum mainboard revision",
-            TYPE            => "mainboard",
-            VERSION         => $mbrev,
-            MANUFACTURER    => "Radware"
-        };
-        $device->addFirmware($firmware);
-    }
-
-    my $hwVersion = getCanonicalString($self->get(hwVersion));
-    if ($hwVersion) {
-        my $firmware = {
-            NAME            => "$device->{MODEL} hardware",
-            DESCRIPTION     => "$device->{MODEL} hardware revision",
-            TYPE            => "device",
-            VERSION         => $hwVersion,
-            MANUFACTURER    => "Radware"
-        };
-        $device->addFirmware($firmware);
-    }
-
-    # Fallback mac address if not set
-    unless ($device->{MAC}) {
-        $device->{MAC} = getCanonicalMacAddress($self->get(hwMACAddress));
-        $device->{INFO}->{MAC} = $device->{MAC};
-    }
-}
-
-1;
-
-__END__
-
-=head1 NAME
-
-GLPI::Agent::SNMP::MibSupport::Radware - Inventory module for Radware devices
-
-=head1 DESCRIPTION
-
-The module enhances Radware devices support.
+        if not device.MAC:
+            device.MAC = get_canonical_mac_address(self.get(HW_MAC_ADDRESS))
+            device.INFO["MAC"] = device.MAC
