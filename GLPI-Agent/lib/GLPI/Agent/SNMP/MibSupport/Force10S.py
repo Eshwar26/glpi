62ab522514ffbd86d3f3a095fe651472f7c76538
Eshwar Thota <162366958+Eshwar26@users.noreply.github.com>
2025-10-27 16:09:27 +0530

    Update Force10S.py

--- GLPI-Agent/lib/GLPI/Agent/SNMP/MibSupport/Force10S.py
+++ GLPI-Agent/lib/GLPI/Agent/SNMP/MibSupport/Force10S.py
@@ -1,178 +1,232 @@
-package GLPI::Agent::SNMP::MibSupport::Force10S;
+import re
 
-use strict;
-use warnings;
+# Mock or simple implementations for GLPI Agent functions/tools
+def get_canonical_constant(value):
+    # Mock: return int value as string
+    try:
+        return str(int(value))
+    except ValueError:
+        return str(value)
 
-use parent 'GLPI::Agent::SNMP::MibSupportTemplate';
+def trim_whitespace(value):
+    if value is None:
+        return None
+    return str(value).strip()
 
-use GLPI::Agent::Tools;
-use GLPI::Agent::Tools::SNMP;
+def get_canonical_string(value):
+    if value is None:
+        return None
+    return trim_whitespace(value)
 
-use constant Force10S => '.1.3.6.1.4.1.6027.1.3' ;
+def get_canonical_mac_address(value):
+    # Mock, not used here
+    return value
 
-our $mibSupport = [
-    {
-        name        => "Force10 S-series",
-        sysobjectid => getRegexpOidMatch(Force10S)
-    }
-];
+def get_canonical_count(value):
+    # Mock: return count as string
+    try:
+        return str(int(value))
+    except ValueError:
+        return str(value)
 
-# See F10-S-SERIES-CHASSIS-MIB
-use constant
-    chStackUnitEntry => '.1.3.6.1.4.1.6027.3.10.1.2.2.1';
+def get_regexp_oid_match(oid):
+    return re.compile(f'^{re.escape(oid)}')
 
-use constant
-    chSysPortIfIndex => '.1.3.6.1.4.1.6027.3.10.1.2.5.1.5';
+# Simple mock base class for compatibility
+class MibSupportTemplate:
+    def __init__(self):
+        self.device = None  # To be set externally; for testing, can be mocked
 
-# components interface variables
-my %physical_components_variables = (
-    INDEX            => { # chStackUnitNumber
-        suffix  => '2',
-        type    => 'integer'
-    },
-    MODEL            => { # chStackUnitModelID
-        suffix  => '7',
-        type    => 'string'
-    },
-    DESCRIPTION      => { # chStackUnitDescription
-        suffix  => '9',
-        type    => 'string'
-    },
-    FIRMWARE         => { # chStackUnitCodeVersion
-        suffix  => '10',
-        type    => 'string'
-    },
-    SERIAL           => { # chStackUnitSerialNumber
-        suffix  => '12',
-        type    => 'string'
-    },
-    REVISION         => { # chStackUnitProductRev
-        suffix  => '21',
-        type    => 'string'
-    },
-);
-
-
-sub getComponents {
-    my ($self) = @_;
-
-    my @components;
-
-    my $stack_components = $self->_get_stack_units();
-    my $ports_components = $self->_get_ports();
-    push @components, @{$stack_components} if $stack_components;
-    push @components, @{$ports_components} if $ports_components;
-
-    # adding root unit
-    if (scalar @components) {
-        push @components, {
-            CONTAINEDININDEX => '0',
-            INDEX            => '-1',
-            TYPE             => 'stack',
-            NAME             => 'Force10 S-series Stack'
-        };
-    }
-
-    return \@components;
-}
-
-sub _get_ports {
-    my ($self) = @_;
-
-    my $walk = $self->walk(chSysPortIfIndex)
-        or return;
-    return unless keys %$walk;
-
-    my @ports;
-    while (my ($suffix, $ifIndex) = each %$walk) {
-        my $stack_id = _getElement($suffix, -2);
-        next unless defined $stack_id;
-
-        push @ports, {
-            INDEX            => $ifIndex,
-            CONTAINEDININDEX => $stack_id,
-            TYPE             => 'port'
-        };
-    }
-
-    return \@ports;
-}
-
-sub _get_stack_units {
-    my ($self) = @_;
-
-    my $walk = $self->walk(chStackUnitEntry)
-        or return;
-    return unless keys %$walk;
-
-    # Parse suffixes to only keep what we really need from the walk
-    my %supported = ();
-    foreach my $key (keys %physical_components_variables) {
-        next unless $physical_components_variables{$key}->{suffix};
-        $supported{$physical_components_variables{$key}->{suffix}} = $key;
-    }
-    my $supported = join '|', sort { $a <=> $b } keys %supported;
-    my $supported_re = qr/^($supported)\.(.*)$/;
-    my %walks = ();
-    foreach my $oidleaf (keys %$walk) {
-        my ($node, $suffix) = $oidleaf =~ $supported_re;
-        next unless defined $node && defined $suffix;
-        $walks{$supported{$node}}->{$suffix} = $walk->{$oidleaf};
-    }
-
-    my @indexes = values %{$walks{INDEX}};
-    return unless @indexes;
-
-    @indexes = sort { $a <=> $b } @indexes;
-
-    # Initialize components array
-    my @components;
-    foreach my $index (@indexes) {
-        my $idx = getCanonicalConstant($walks{INDEX}->{$index} || $index);
-        push @components, {
-            INDEX            => $idx,
-            # minimal chassis number in an interface name is zero, e.g. Gi0/1
-            NAME             => $idx - 1,
-            CONTAINEDININDEX => '-1',
-            TYPE             => 'chassis',
-        };
-    };
-
-    my @keys = sort grep {$_ ne 'INDEX'} keys %physical_components_variables;
-
-    # Populate all components
-    my $i = 0;
-    while ($i < scalar @indexes) {
-        my $component = $components[$i];
-        my $index     = $indexes[$i++];
-
-        foreach my $key (@keys) {
-            my $variable  = $physical_components_variables{$key};
-            my $type      = $variable->{type} || '';
-            my $raw_value = $walks{$key}->{$index};
-            next unless defined $raw_value;
-            my $value =
-                $type eq 'type'     ? $variable->{types}->{$raw_value}   :
-                $type eq 'mac'      ? getCanonicalMacAddress($raw_value) :
-                $type eq 'constant' ? getCanonicalConstant($raw_value)   :
-                $type eq 'string'   ? getCanonicalString(trimWhitespace($raw_value)) :
-                $type eq 'count'    ? getCanonicalCount($raw_value)      :
-                                      $raw_value;
-            $component->{$key} = $value
-                if defined($value) && length($value);
+    def walk(self, oid):
+        # Mock SNMP walk; returns dict of full_oid_suffix: value
+        mock_walks = {
+            '.1.3.6.1.4.1.6027.3.10.1.2.2.1': {  # chStackUnitEntry walk mock
+                '2.1': '1',  # INDEX suffix 2, index 1
+                '7.1': 'S4810',  # MODEL suffix 7
+                '9.1': 'Force10 S4810',  # DESCRIPTION suffix 9
+                '10.1': '5.6.1.0',  # FIRMWARE suffix 10
+                '12.1': 'SN123456',  # SERIAL suffix 12
+                '21.1': 'A01',  # REVISION suffix 21
+                '2.2': '2',  # INDEX for unit 2
+                '7.2': 'S4820',  # MODEL for unit 2
+            },
+            '.1.3.6.1.4.1.6027.3.10.1.2.5.1.5': {  # chSysPortIfIndex
+                '1.1': '1',  # suffix for port on unit 1
+                '1.2': '2',  # another port
+            }
         }
+        # Simulate full OID suffixes
+        base = oid.replace('.1.3.6.1.4.1.6027.3.10', '')  # Adjust for mock
+        return mock_walks.get(oid, {})
+
+# Constants
+FORCE10S = '.1.3.6.1.4.1.6027.1.3'
+
+CH_STACK_UNIT_ENTRY = '.1.3.6.1.4.1.6027.3.10.1.2.2.1'
+
+CH_SYS_PORT_IF_INDEX = '.1.3.6.1.4.1.6027.3.10.1.2.5.1.5'
+
+physical_components_variables = {
+    'INDEX': {
+        'suffix': '2',
+        'type': 'integer'
+    },
+    'MODEL': {
+        'suffix': '7',
+        'type': 'string'
+    },
+    'DESCRIPTION': {
+        'suffix': '9',
+        'type': 'string'
+    },
+    'FIRMWARE': {
+        'suffix': '10',
+        'type': 'string'
+    },
+    'SERIAL': {
+        'suffix': '12',
+        'type': 'string'
+    },
+    'REVISION': {
+        'suffix': '21',
+        'type': 'string'
+    },
+}
+
+mib_support = [
+    {
+        'name': "Force10 S-series",
+        'sysobjectid': get_regexp_oid_match(FORCE10S)
     }
+]
 
-    return \@components;
-}
+class Force10S(MibSupportTemplate):
+    def get_components(self):
+        components = []
 
-sub _getElement {
-    my ($oid, $index) = @_;
+        stack_components = self._get_stack_units()
+        if stack_components:
+            components.extend(stack_components)
 
-    my @array = split(/\./, $oid);
-    return $array[$index];
-}
+        ports_components = self._get_ports()
+        if ports_components:
+            components.extend(ports_components)
 
-1;
+        # adding root unit
+        if components:
+            components.append({
+                'CONTAINEDININDEX': '0',
+                'INDEX': '-1',
+                'TYPE': 'stack',
+                'NAME': 'Force10 S-series Stack'
+            })
 
-__END__
+        return components
+
+    def _get_ports(self):
+        walk = self.walk(CH_SYS_PORT_IF_INDEX)
+        if not walk:
+            return None
+
+        ports = []
+        for suffix, if_index in walk.items():
+            stack_id = _get_element(suffix, -2)
+            if stack_id is not None:
+                ports.append({
+                    'INDEX': if_index,
+                    'CONTAINEDININDEX': stack_id,
+                    'TYPE': 'port'
+                })
+
+        return ports if ports else None
+
+    def _get_stack_units(self):
+        walk = self.walk(CH_STACK_UNIT_ENTRY)
+        if not walk:
+            return None
+
+        # Parse suffixes to only keep what we really need from the walk
+        supported = {}
+        for key in physical_components_variables:
+            suffix = physical_components_variables[key].get('suffix')
+            if suffix:
+                supported[suffix] = key
+
+        supported_suffixes = sorted(supported.keys(), key=int)
+        supported_re = re.compile(f'^({'|'.join(supported_suffixes)})\.(.*)$')
+        walks = {}
+        for oid_leaf in walk:
+            match = supported_re.match(oid_leaf)
+            if match:
+                node, suffix = match.groups()
+                key = supported[node]
+                if key not in walks:
+                    walks[key] = {}
+                walks[key][suffix] = walk[oid_leaf]
+
+        index_walk = walks.get('INDEX', {})
+        indexes = list(index_walk.values())
+        if not indexes:
+            return None
+
+        indexes = sorted(int(i) for i in indexes)
+
+        # Initialize components array
+        components = []
+        for idx in indexes:
+            index_str = get_canonical_constant(index_walk.get(str(idx), str(idx)))
+            components.append({
+                'INDEX': index_str,
+                # minimal chassis number in an interface name is zero, e.g. Gi0/1
+                'NAME': str(int(index_str) - 1),
+                'CONTAINEDININDEX': '-1',
+                'TYPE': 'chassis',
+            })
+
+        keys = sorted(k for k in physical_components_variables if k != 'INDEX')
+
+        # Populate all components
+        i = 0
+        while i < len(indexes):
+            component = components[i]
+            index = indexes[i]
+
+            for key in keys:
+                variable = physical_components_variables[key]
+                type_val = variable.get('type', '')
+                raw_value = walks.get(key, {}).get(str(index))
+                if raw_value is None:
+                    continue
+
+                if type_val == 'type':
+                    value = variable.get('types', {}).get(raw_value)
+                elif type_val == 'mac':
+                    value = get_canonical_mac_address(raw_value)
+                elif type_val == 'constant':
+                    value = get_canonical_constant(raw_value)
+                elif type_val == 'string':
+                    value = get_canonical_string(trim_whitespace(raw_value))
+                elif type_val == 'count':
+                    value = get_canonical_count(raw_value)
+                else:
+                    value = raw_value
+
+                if value is not None and len(str(value)) > 0:
+                    component[key] = value
+
+            i += 1
+
+        return components
+
+def _get_element(oid, index_pos):
+    array = oid.split('.')
+    return array[index_pos] if 0 <= index_pos < len(array) else None
+
+# For testing/standalone run (optional)
+if __name__ == "__main__":
+    # Test instantiation
+    force10s = Force10S()
+    components = force10s.get_components()
+    print("Components:", components)
+    print("Module loaded and run successfully without errors.")
+    
