0f10bb32108694cab1d7b55c9561c1f8207730a1
Eshwar Thota <162366958+Eshwar26@users.noreply.github.com>
2025-10-27 16:20:58 +0530

    Update Zyxel.py

--- GLPI-Agent/lib/GLPI/Agent/SNMP/MibSupport/Zyxel.py
+++ GLPI-Agent/lib/GLPI/Agent/SNMP/MibSupport/Zyxel.py
@@ -1,66 +1,57 @@
-package GLPI::Agent::SNMP::MibSupport::Zyxel;
+# -*- coding: utf-8 -*-
+"""
+GLPI.Agent.SNMP.MibSupport.Zyxel
+Inventory module for Zyxel devices
+"""
 
-use strict;
-use warnings;
+from GLPI.Agent.SNMP.MibSupportTemplate import MibSupportTemplate
+from GLPI.Agent.Tools import getCanonicalString
+from GLPI.Agent.Tools.SNMP import getRegexpOidMatch
 
-use parent 'GLPI::Agent::SNMP::MibSupportTemplate';
+class Zyxel(MibSupportTemplate):
+    priority = 5
 
-use GLPI::Agent::Tools;
-use GLPI::Agent::Tools::SNMP;
+    # SNMP OIDs
+    zyxel = '.1.3.6.1.4.1.890'
+    products = zyxel + '.1'
+    enterpriseSolution = products + '.15'
+    esMgmt = enterpriseSolution + '.3'
 
-use constant    priority => 5;
+    # ZYXEL-ES-COMMON MIB OIDs
+    esSysInfo = esMgmt + '.1'
+    sysSwVersionString = esSysInfo + '.6.0'
+    sysProductModel = esSysInfo + '.11.0'
+    sysProductSerialNumber = esSysInfo + '.12.0'
 
-# See ZYXEL-ES-SMI
-use constant    zyxel => '.1.3.6.1.4.1.890' ;
-use constant    products => zyxel . '.1';
-use constant    enterpriseSolution => products . '.15';
-use constant    esMgmt => enterpriseSolution . '.3';
+    mibSupport = [
+        {
+            "name": "zyxel",
+            "sysobjectid": getRegexpOidMatch(enterpriseSolution)
+        }
+    ]
 
-# From ZYXEL-ES-COMMON
-use constant    esSysInfo => esMgmt . '.1' ;
-use constant    sysSwVersionString     => esSysInfo . '.6.0' ;
-use constant    sysProductModel        => esSysInfo . '.11.0' ;
-use constant    sysProductSerialNumber => esSysInfo . '.12.0' ;
+    def getFirmware(self):
+        """Return firmware version string."""
+        return getCanonicalString(self.get(self.sysSwVersionString))
 
-our $mibSupport = [
-    {
-        name        => "zyxel",
-        sysobjectid => getRegexpOidMatch(enterpriseSolution)
-    }
-];
+    def getSerial(self):
+        """Return device serial number."""
+        return getCanonicalString(self.get(self.sysProductSerialNumber))
 
-sub getFirmware {
-    my ($self) = @_;
+    def getManufacturer(self):
+        """Return manufacturer name."""
+        return "Zyxel"
 
-    return getCanonicalString($self->get(sysSwVersionString));
-}
+    def getModel(self):
+        """Return product model string."""
+        return getCanonicalString(self.get(self.sysProductModel))
 
-sub getSerial {
-    my ($self) = @_;
 
-    return getCanonicalString($self->get(sysProductSerialNumber));
-}
+# --- Module Metadata ---
+"""
+NAME
+    GLPI.Agent.SNMP.MibSupport.Zyxel - Inventory module for Zyxel devices
 
-sub getManufacturer {
-    my ($self) = @_;
-
-    return "Zyxel";
-}
-
-sub getModel {
-    my ($self) = @_;
-
-    return getCanonicalString($self->get(sysProductModel));
-}
-
-1;
-
-__END__
-
-=head1 NAME
-
-GLPI::Agent::SNMP::MibSupport::Zyxel - Inventory module for Zyxel devices
-
-=head1 DESCRIPTION
-
-The module enhances Zyxel devices support.
+DESCRIPTION
+    Enhances SNMP-based inventory collection for Zyxel devices.
+"""
