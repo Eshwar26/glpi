215aeafd47e87ab713a23987ac7f8221fc437456
Eshwar Thota <162366958+Eshwar26@users.noreply.github.com>
2025-10-27 15:57:19 +0530

    Update Components.py

--- GLPI-Agent/lib/GLPI/Agent/SNMP/Device/Components.py
+++ GLPI-Agent/lib/GLPI/Agent/SNMP/Device/Components.py
@@ -1,258 +1,228 @@
-package GLPI::Agent::SNMP::Device::Components;
+import re
+from typing import Dict, List, Any, Optional
 
-use strict;
-use warnings;
-
-use GLPI::Agent::Tools;
-use GLPI::Agent::Tools::SNMP;
+# Assuming GLPIAgentTools and GLPIAgentToolsSNMP are available
+from glpi_agent_tools import getCanonicalConstant, getCanonicalString, trimWhitespace, getCanonicalMacAddress, getCanonicalSerialNumber, getCanonicalCount
+from glpi_agent_tools_snmp import *
 
 # See ENTITY-MIB
-use constant
-    entPhysicalEntry    => '.1.3.6.1.2.1.47.1.1.1.1';
+ENT_PHYSICAL_ENTRY = '.1.3.6.1.2.1.47.1.1.1.1'
 
 # See Dell-Vendor-MIB
-use constant
-    productIdentificationSerialNumber   => '.1.3.6.1.4.1.674.10895.3000.1.2.100.8.1.2';
+PRODUCT_IDENTIFICATION_SERIAL_NUMBER = '.1.3.6.1.4.1.674.10895.3000.1.2.100.8.1.2'
 
 # components interface variables
-my %physical_components_variables = (
-    INDEX            => { # entPhysicalIndex
-        suffix  => '1',
-        type    => 'constant'
+PHYSICAL_COMPONENTS_VARIABLES = {
+    'INDEX': {  # entPhysicalIndex
+        'suffix': '1',
+        'type': 'constant'
     },
-    NAME             => { # entPhysicalName
-        suffix  => '7',
-        type    => 'string'
+    'NAME': {  # entPhysicalName
+        'suffix': '7',
+        'type': 'string'
     },
-    DESCRIPTION      => { # entPhysicalDescr
-        suffix  => '2',
-        type    => 'string'
+    'DESCRIPTION': {  # entPhysicalDescr
+        'suffix': '2',
+        'type': 'string'
     },
-    SERIAL           => { # entPhysicalSerialNum
-        suffix  => '11',
-        type    => 'string'
+    'SERIAL': {  # entPhysicalSerialNum
+        'suffix': '11',
+        'type': 'string'
     },
-    MODEL            => { # entPhysicalModelName
-        suffix  => '13',
-        type    => 'string'
+    'MODEL': {  # entPhysicalModelName
+        'suffix': '13',
+        'type': 'string'
     },
-    TYPE             => { # entPhysicalClass
-        suffix  => '5',
-        type => 'type',
-        types => {
-           'other(1)'       => 'other',
-           1                => 'other',
-           'unknown(2)'     => 'unknown',
-           2                => 'unknown',
-           'chassis(3)'     => 'chassis',
-           3                => 'chassis',
-           'backplane(4)'   => 'backplane',
-           4                => 'backplane',
-           'container(5)'   => 'container',
-           5                => 'container',
-           'powerSupply(6)' => 'powerSupply',
-           6                => 'powerSupply',
-           'fan(7)'         => 'fan',
-           7                => 'fan',
-           'sensor(8)'      => 'sensor',
-           8                => 'sensor',
-           'module(9)'      => 'module',
-           9                => 'module',
-           'port(10)'       => 'port',
-           10               => 'port',
-           'stack(11)'      => 'stack',
-           11               => 'stack',
-           'cpu(12)'        => 'cpu',
-           12               => 'cpu'
+    'TYPE': {  # entPhysicalClass
+        'suffix': '5',
+        'type': 'type',
+        'types': {
+            'other(1)': 'other',
+            1: 'other',
+            'unknown(2)': 'unknown',
+            2: 'unknown',
+            'chassis(3)': 'chassis',
+            3: 'chassis',
+            'backplane(4)': 'backplane',
+            4: 'backplane',
+            'container(5)': 'container',
+            5: 'container',
+            'powerSupply(6)': 'powerSupply',
+            6: 'powerSupply',
+            'fan(7)': 'fan',
+            7: 'fan',
+            'sensor(8)': 'sensor',
+            8: 'sensor',
+            'module(9)': 'module',
+            9: 'module',
+            'port(10)': 'port',
+            10: 'port',
+            'stack(11)': 'stack',
+            11: 'stack',
+            'cpu(12)': 'cpu',
+            12: 'cpu'
         }
     },
-    FRU              => { # entPhysicalIsFRU
-        suffix  => '16',
-        type    => 'constant'
+    'FRU': {  # entPhysicalIsFRU
+        'suffix': '16',
+        'type': 'constant'
     },
-    MANUFACTURER     => { # entPhysicalMfgName
-        suffix  => '12',
-        type    => 'string'
+    'MANUFACTURER': {  # entPhysicalMfgName
+        'suffix': '12',
+        'type': 'string'
     },
-    FIRMWARE         => { # entPhysicalFirmwareRev
-        suffix  => '9',
-        type    => 'string'
+    'FIRMWARE': {  # entPhysicalFirmwareRev
+        'suffix': '9',
+        'type': 'string'
     },
-    REVISION         => { # entPhysicalHardwareRev
-        suffix  => '8',
-        type    => 'string'
+    'REVISION': {  # entPhysicalHardwareRev
+        'suffix': '8',
+        'type': 'string'
     },
-    VERSION          => { # entPhysicalSoftwareRev
-        suffix  => '10',
-        type    => 'string'
+    'VERSION': {  # entPhysicalSoftwareRev
+        'suffix': '10',
+        'type': 'string'
     },
-    CONTAINEDININDEX => { # entPhysicalContainedIn
-        suffix  => '4',
-        type    => 'constant'
+    'CONTAINEDININDEX': {  # entPhysicalContainedIn
+        'suffix': '4',
+        'type': 'constant'
     },
-    MAC => {
-        type => 'mac'
+    'MAC': {
+        'type': 'mac'
     },
-    IP => {
-        type => 'string'
+    'IP': {
+        'type': 'string'
     },
-);
-
-sub new {
-    my ($class, %params) = @_;
-
-    my $device = $params{device};
-
-    return unless $device;
-
-    my $self = {
-        device      => $device,
-        _components => [],
-    };
-
-    # First walk all entPhysicalEntry entries
-    my $walk = $device->walk(entPhysicalEntry)
-        or return;
-    return unless keys(%{$walk});
-
-    # Parse suffixes to only keep what we really need from the walk
-    my %supported = ();
-    foreach my $key (keys(%physical_components_variables)) {
-        next unless $physical_components_variables{$key}->{suffix};
-        $supported{$physical_components_variables{$key}->{suffix}} = $key;
-    }
-    my $supported = join('|',sort { $a <=> $b } keys(%supported));
-    my $supported_re = qr/^($supported)\.(.*)$/;
-    my %walks = ();
-    foreach my $oidleaf (keys(%{$walk})) {
-        my ( $node, $suffix ) = $oidleaf =~ $supported_re;
-        next unless defined $node && defined $suffix;
-        $walks{$supported{$node}}->{$suffix} = $walk->{$oidleaf};
-    }
-
-    # No instanciation if no indexed component found by INDEX or based on NAME
-    my @indexes;
-    if ($walks{INDEX}) {
-        # Trust INDEX table when present
-        @indexes = values(%{$walks{INDEX}});
-    } else {
-        # Found the most populated info and use related suffixes as index table
-        my %counts = map { $_ => scalar(keys(%{$walks{$_}})) } keys(%walks);
-        my @larger = sort { $counts{$a} <=> $counts{$b} } keys(%walks);
-        my $larger = pop @larger;
-        @indexes = keys(%{$walks{$larger}});
-    }
-    return unless @indexes;
-
-    @indexes = sort { $a <=> $b } @indexes;
-
-    # Checking MAC & IP are for now only supported for Cisco based devices
-    my $mac_indexes = $device->walk('.1.3.6.1.4.1.9.9.513.1.1.1.1.4');
-    if ($mac_indexes) {
-        # Get MAC addresses
-        my $macaddresses = $device->walk('.1.3.6.1.4.1.9.9.513.1.1.1.1.2')
-            || {};
-        # Get IP addresses
-        my $ipaddresses = $device->walk('.1.3.6.1.4.1.14179.2.2.1.1.19')
-            || {};
-
-        # Populate MAC & IP addresses
-        while (my ($suffix, $index) = each %{$mac_indexes}) {
-            $walks{MAC}->{$index} = $macaddresses->{$suffix}
-                if $macaddresses->{$suffix};
-            $walks{IP}->{$index} = $ipaddresses->{$suffix}
-                if $ipaddresses->{$suffix};
-        }
-    }
-
-    # Initialize _components array
-    foreach my $index (@indexes) {
-        push @{$self->{_components}}, {
-            INDEX => getCanonicalConstant($walks{INDEX}->{$index} || $index)
-        };
-    };
-
-    # Dell chassis serialnumbers should be retrieved from a private oid
-    my $dellSerialNumbers = $device->walk(productIdentificationSerialNumber);
-    $self->{_dellSN} = $dellSerialNumbers
-        if $dellSerialNumbers && keys(%{$dellSerialNumbers}) > 1;
-
-    $self->{_indexes} = \@indexes;
-    $self->{_walks}   = \%walks;
-
-    bless $self, $class;
-
-    return $self;
 }
 
-sub getPhysicalComponents {
-    my ($self) = @_;
 
-    # INDEX was still computed during object creation
-    my @keys = sort grep { $_ ne 'INDEX' } keys(%physical_components_variables);
+class GLPIAgentSNMPDeviceComponents:
+    """
+    GLPI agent SNMP device components
 
-    my $i = 0;
-    my $count = @{$self->{_indexes}};
-    my $module = 0;
+    Class to help handle components method for snmp device inventory
+    """
 
-    # Populate all components
-    while ($i < $count) {
-        my $component = $self->{_components}->[$i];
-        my $index     = $self->{_indexes}->[$i++];
+    def __new__(cls, **params):
+        device = params.get('device')
+        if not device:
+            return None
 
-        foreach my $key (@keys) {
-            my $variable  = $physical_components_variables{$key};
-            my $type      = $variable->{type} || '';
-            my $raw_value = $self->{_walks}->{$key}->{$index};
-            next unless defined $raw_value;
-            my $value =
-                $type eq 'type'     ? $variable->{types}->{$raw_value}   :
-                $type eq 'mac'      ? getCanonicalMacAddress($raw_value) :
-                $type eq 'constant' ? getCanonicalConstant($raw_value)   :
-                $type eq 'string'   ? getCanonicalString(trimWhitespace($raw_value)) :
-                $type eq 'count'    ? getCanonicalCount($raw_value)      :
-                                      $raw_value;
-            $component->{$key} = $value
-                if defined($value) && length($value);
-        }
+        self = super().__new__(cls)
+        self.device = device
+        self._components = []
 
-        # Fix Chassis S/N for Dell devices
-        if ($self->{_dellSN} && $component->{TYPE} && $component->{TYPE} eq 'chassis') {
-            if ($component->{NAME} && $component->{NAME} =~ /^Unit (\d+)$/) {
-                $module = int($1);
-            } else {
-                $module++;
-            }
-            my $serial = getCanonicalSerialNumber($self->{_dellSN}->{$module});
-            $component->{SERIAL} = $serial
-                if $serial && (!$component->{SERIAL} || $component->{SERIAL} ne $serial);
-        }
-    }
+        # First walk all entPhysicalEntry entries
+        walk = device.walk(ENT_PHYSICAL_ENTRY)
+        if not walk:
+            return None
+        if not walk:
+            return None
 
-    return $self->{_components};
-}
+        # Parse suffixes to only keep what we really need from the walk
+        supported = {}
+        for key in PHYSICAL_COMPONENTS_VARIABLES:
+            if 'suffix' in PHYSICAL_COMPONENTS_VARIABLES[key]:
+                supported[PHYSICAL_COMPONENTS_VARIABLES[key]['suffix']] = key
+        supported_suffixes = sorted(supported.keys(), key=int)
+        supported_str = '|'.join(supported_suffixes)
+        supported_re = re.compile(rf'^({supported_str})\.(.+)$')
+        walks = {}
+        for oidleaf in walk:
+            match = supported_re.match(oidleaf)
+            if match:
+                node, suffix = match.groups()
+                walks.setdefault(supported[node], {})[suffix] = walk[oidleaf]
 
-1;
+        # No instanciation if no indexed component found by INDEX or based on NAME
+        indexes = []
+        if 'INDEX' in walks:
+            # Trust INDEX table when present
+            indexes = list(walks['INDEX'].values())
+        else:
+            # Found the most populated info and use related suffixes as index table
+            counts = {k: len(v) for k, v in walks.items()}
+            larger = max(counts, key=counts.get)
+            indexes = list(walks[larger].keys())
+        if not indexes:
+            return None
 
-__END__
+        indexes = sorted(indexes, key=int)
 
-=head1 NAME
+        # Checking MAC & IP are for now only supported for Cisco based devices
+        mac_indexes_oid = '.1.3.6.1.4.1.9.9.513.1.1.1.1.4'
+        mac_indexes = device.walk(mac_indexes_oid)
+        if mac_indexes:
+            # Get MAC addresses
+            macaddresses_oid = '.1.3.6.1.4.1.9.9.513.1.1.1.1.2'
+            macaddresses = device.walk(macaddresses_oid) or {}
+            # Get IP addresses
+            ipaddresses_oid = '.1.3.6.1.4.1.14179.2.2.1.1.19'
+            ipaddresses = device.walk(ipaddresses_oid) or {}
 
-GLPI::Agent::SNMP::Device::Components - GLPI agent SNMP device components
+            # Populate MAC & IP addresses
+            for suffix, index in mac_indexes.items():
+                if suffix in macaddresses:
+                    walks.setdefault('MAC', {})[index] = macaddresses[suffix]
+                if suffix in ipaddresses:
+                    walks.setdefault('IP', {})[index] = ipaddresses[suffix]
 
-=head1 DESCRIPTION
+        # Initialize _components array
+        for index in indexes:
+            self._components.append({
+                'INDEX': getCanonicalConstant(walks.get('INDEX', {}).get(index, index))
+            })
 
-Class to help handle components method for snmp device inventory
+        # Dell chassis serialnumbers should be retrieved from a private oid
+        dell_serial_numbers = device.walk(PRODUCT_IDENTIFICATION_SERIAL_NUMBER)
+        self._dellSN = dell_serial_numbers if dell_serial_numbers and len(dell_serial_numbers) > 1 else None
 
-=head1 METHODS
+        self._indexes = indexes
+        self._walks = walks
 
-=head2 new(%params)
+        return self
 
-The constructor. The following parameters are allowed, as keys of the %params
-hash:
+    def get_physical_components(self):
+        # INDEX was still computed during object creation
+        keys = sorted([k for k in PHYSICAL_COMPONENTS_VARIABLES if k != 'INDEX'])
 
-=over
+        count = len(self._indexes)
+        module = 0
 
-=item device (mandatory)  Device related object
+        # Populate all components
+        for i in range(count):
+            component = self._components[i]
+            index = self._indexes[i]
 
-=back
+            for key in keys:
+                variable = PHYSICAL_COMPONENTS_VARIABLES[key]
+                type_ = variable.get('type', '')
+                raw_value = self._walks.get(key, {}).get(index)
+                if raw_value is None:
+                    continue
+                if type_ == 'type':
+                    value = variable['types'].get(raw_value)
+                elif type_ == 'mac':
+                    value = getCanonicalMacAddress(raw_value)
+                elif type_ == 'constant':
+                    value = getCanonicalConstant(raw_value)
+                elif type_ == 'string':
+                    value = getCanonicalString(trimWhitespace(raw_value))
+                elif type_ == 'count':
+                    value = getCanonicalCount(raw_value)
+                else:
+                    value = raw_value
+                if value is not None and len(str(value)) > 0:
+                    component[key] = value
+
+            # Fix Chassis S/N for Dell devices
+            if self._dellSN and component.get('TYPE') == 'chassis':
+                if component.get('NAME') and re.match(r'^Unit (\d+)$', component['NAME']):
+                    module = int(re.match(r'^Unit (\d+)$', component['NAME']).group(1))
+                else:
+                    module += 1
+                serial = getCanonicalSerialNumber(self._dellSN.get(module))
+                if serial and (component.get('SERIAL') is None or component['SERIAL'] != serial):
+                    component['SERIAL'] = serial
+
+        return self._components
